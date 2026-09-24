578de2b8a41945b7f74a7c65e17f16e8678bfab1
Yan <t89601213019@gmail.com>
2026-09-25 00:22:01 +0300

    delt3

--- Task3/Program.java
+++
@@ -1,7 +0,0 @@
-package Task3;
-
-import java.util.Locale;
-import java.util.Scanner;
-
-public class Program {
-    
--- Task3/SimpleColor.java
+++
@@ -1,5 +0,0 @@
-package Task3;
-
-public enum SimpleColor {
-    BLACK, WHITE, GRAY, RED, ORANGE, YELLOW, GREEN, BLUE
-}
